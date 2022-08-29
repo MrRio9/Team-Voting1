0be90463a1d9bdca629e378c7019afd19c73eafd
MrRio9 <83383974+MrRio9@users.noreply.github.com>
2022-08-28 18:12:41 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAe8kQwh045jY_sp-ALIE8zQT6EoYXBPOA",
+    authDomain: "project66-36c06.firebaseapp.com",
+    projectId: "project66-36c06",
+    storageBucket: "project66-36c06.appspot.com",
+    messagingSenderId: "1059683111551",
+    appId: "1:1059683111551:web:3aed9f5bd60a73a94d254a"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
